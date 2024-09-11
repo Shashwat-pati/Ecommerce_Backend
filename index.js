fdd90454b1dd192c621e32537b90d97563b008c1
Shashwat Pati <shashwatpati02@gmail.com>
2024-09-11 12:47:56 +0530

    token

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ app.use(cors());
 app.use((req, res, next) => {
     res.header(
         "Access-Control-Allow-Origin",
-        "https://ecommerce-teal-nu.vercel.app/"
+        "https://ecommerce-teal-nu.vercel.app"
     );
     res.header("Access-Control-Allow-Credentials", "true");
     next();
